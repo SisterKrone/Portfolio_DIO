bfce5dbe507e3301006078c5d8733f2030cd56f5
SisterKrone <luiz.fabiana12@gmail.com>
2025-10-06 21:50:28 -0300

    Add path

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,5 +1,5 @@
 async function fetchProfileData() {
-    const url = 'https://raw.githubusercontent.com/SisterKrone/DIO-Javascript-challenges/refs/heads/main/portfolio/data/profile.json'
+    const url = 'https://raw.githubusercontent.com/SisterKrone/Portfolio_DIO/refs/heads/main/data/profile.json'
     const fetching = await fetch(url)       
     return await fetching.json()    
 }
